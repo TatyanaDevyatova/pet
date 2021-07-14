e76fa328150d30447346ad763ac0cd345df3946b
Tatyana Devyatova <tatyana.v.devyatova@gmail.com>
2021-07-14 18:03:12 +0400

    new pet. day 1

---
+++ src/main/java/leel/exercise/pet/configuration/GreetingRouter.java
@@ -0,0 +1,21 @@
+package leel.exercise.pet.configuration;
+
+import leel.exercise.pet.handler.GreetingHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        RequestPredicate route = RequestPredicates
+                .GET("/hello")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
+
+        return RouterFunctions
+                .route(route, greetingHandler::hello);
+    }
+}
