11790e75819c120040f523eea095867790f17fbc
Tatyana Devyatova <tatyana.v.devyatova@gmail.com>
2021-07-22 16:17:40 +0400

    first video end

--- src/main/java/leel/exercise/pet/configuration/GreetingRouter.java
+++ src/main/java/leel/exercise/pet/configuration/GreetingRouter.java
@@ -4,18 +4,27 @@ import leel.exercise.pet.handler.GreetingHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
+
+import java.util.Map;
 
 @Configuration
 public class GreetingRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+    public RouterFunction<ServerResponse> route(
+            GreetingHandler greetingHandler
+    ) {
         RequestPredicate route = RequestPredicates
                 .GET("/hello")
                 .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
 
         return RouterFunctions
-                .route(route, greetingHandler::hello);
+                .route(route, greetingHandler::hello)
+                .andRoute(
+                        RequestPredicates.GET("/"),
+                        greetingHandler::index);
     }
+
 }
---
+++ src/main/java/leel/exercise/pet/domain/Message.java
@@ -0,0 +1,13 @@
+package leel.exercise.pet.domain;
+
+public class Message {
+    private String data;
+
+    public Message(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
